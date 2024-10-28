cf8800c00bc65aefcba4f521a42fea616187c486
codewithhuzaifa <huzaifaarab2002@gmail.com>
2024-10-28 17:27:15 +0500

    CICD With Firebase App Distribution

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter CICD with App Firebase'),
+      home: const MyHomePage(title: 'Flutter CICD'),
     );
   }
 }
